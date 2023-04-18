f967b76cc637d74edbf59ac5d850064ee3702204
DBogomolov052 <129858974+DBogomolov052@users.noreply.github.com>
2023-04-18 12:53:50 +0300

    Update flower_sorter.py

--- flower_sorter.py
+++ flower_sorter.py
@@ -18,3 +18,19 @@ hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 # Ищем цвета
 mask_red = cv2.inRange(hsv_img, lower_red, upper_red)
 mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
+mask_blue = cv2.inRange(hsv_img, lower_blue, upper_blue)
+
+# Объединяем маски
+mask = cv2.bitwise_or(mask_red, mask_green)
+mask = cv2.bitwise_or(mask, mask_blue)
+
+# Находим контуры
+contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Рисуем контуры на исходном изображении
+for cnt in contours:
+    x,y,w,h = cv2.boundingRect(cnt)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+# Сохраняем результат
+cv2.imwrite("flower_sorter_result.jpg", img)
